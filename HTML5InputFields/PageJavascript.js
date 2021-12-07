e03478e92f4838da788d26d9affcab5713c97eeb
konika <konikasingh1996@gmail.com>
2021-12-07 11:36:03 +0530

    all input fields are validating the rules and in right fomat

--- HTML5InputFields/PageJavascript.js
+++ HTML5InputFields/PageJavascript.js
@@ -1,3 +1,4 @@
+//UC1
 const text=document.querySelector('#text');
 const textError=document.querySelector('.text-error');
 text.addEventListener('input',function()
@@ -10,6 +11,25 @@ text.addEventListener('input',function()
      else
      {
          textError.textContent="name is incorrect";
+
+     }
+
+
+});
+
+//UC2
+const text1=document.querySelector('#email');
+const textError1=document.querySelector('.text-error1');
+text1.addEventListener('input',function()
+{
+     let nameRegex=RegExp('^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][0-9a-zA-Z]+[.][a-zA-Z]{2,4}([a-zA-Z]{2,3}){0,1}');
+     if(nameRegex.test(text1.value))
+     {  
+         textError1.textContent=" ";
+     }
+     else
+     {
+         textError1.textContent="incorrect email";
 
      }
 
